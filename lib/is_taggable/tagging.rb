52569de2deea90b2684caa2bdc8a97e461858853
Ben Alavi <benalavi@gmail.com>
2008-09-26 15:57:30 -0700

    fixed up tagging spec

--- lib/is_taggable/tagging.rb
+++ lib/is_taggable/tagging.rb
@@ -1,10 +1,10 @@
 class Tagging < ActiveRecord::Base #:nodoc:
   belongs_to :taggable, :polymorphic => true
   belongs_to :tagger, :polymorphic => true
-
+  
   validates_presence_of :context
   validates_presence_of :tag
-  validates_uniqueness_of :tag, :with_scope => [ :context, :taggable_type, :taggable_id ]
+  validates_uniqueness_of :tag, :scope => [ :context, :taggable_type, :taggable_id ]
   
   # LIKE is used for cross-database case-insensitivity
   class << self
@@ -12,6 +12,10 @@ class Tagging < ActiveRecord::Base #:nodoc:
       find(:first, :conditions => ["tag LIKE ?", tag]) || create(:tag => tag)
     end
     alias :find_or_create_with_like_by_name :find_or_create_with_like_by_tag
+  end
+  
+  def context
+    self[:context] ? self[:context].to_sym : nil
   end
   
   def to_s
--- spec/is_taggable/tagging_spec.rb
+++ spec/is_taggable/tagging_spec.rb
@@ -5,20 +5,19 @@ describe Tagging do
     @tagging = Tagging.new
   end
 
-  it "should require a name" do
-    @tag.should have(1).errors_on(:name)
-    @tag.name = "something"
-    @tag.should have(0).errors_on(:name)    
+  it "should require a tag" do
+    @tagging.valid?
+    @tagging.should have(1).errors_on(:tag)
   end
   
-  it "should equal a tag with the same name" do
-    @tag.name = "awesome"
-    new_tag = Tag.new(:name => "awesome")
-    new_tag.should == @tag
+  it "should be valid with a tag" do
+    @tagging.tag = "something"
+    @tagging.valid?
+    @tagging.should have(0).errors_on(:tag)
   end
   
   it "should return its name when to_s is called" do
-    @tag.name = "cool"
-    @tag.to_s.should == "cool"
+    @tagging.tag = "cool"
+    @tagging.to_s.should == "cool"
   end
 end
